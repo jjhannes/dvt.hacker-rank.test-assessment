e77d895233f20dc1d198d891265690571ae4f683
Hannes <that.other.guy.i.know@gmail.com>
2022-06-15 09:03:38 +0200

    Changes - Some site styling - Include bootstrap - Update test command + watch - Ignore test output - Define landing page + title style - Update app component tests - Basic app component behavior

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'musicdb-app-angular'`, () => {
+  it(`should have as title 'DeeVerT'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('musicdb-app-angular');
+    expect(app.title).toEqual('DeeVerT');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('musicdb-app-angular app is running!');
+    expect(compiled.querySelector('.landing-title').textContent).toContain('DeeVerT');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'musicdb-app-angular';
+
+  title = 'DeeVerT';
+
+  search() {
+    
+    // query deezer service
+
+  }
+
 }
